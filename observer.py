142569ea097d56a24bcc104773ae26fb3a763948
Knut Zuidema <knut.zuidema@gmail.com>
2018-09-09 01:27:10 +0200

    catch praw exception and continue after wait

--- observer.py
+++ observer.py
@@ -2,7 +2,7 @@ import json
 import time
 from collections import defaultdict
 import logging
-import re
+from prawcore import PrawcoreException
 
 from praw import Reddit
 
@@ -29,49 +29,53 @@ if __name__ == '__main__':
     iterations = config.get('iterations', 0)
     try:
         while True:
-            iterations += 1
-            logging.info(f'Observer iteration {iterations}')
-            for subreddit in subreddits:
-                logging.info(f'  Observing subreddit {subreddit}')
-                for submission in reddit.subreddit(subreddit).new(
-                        limit=depth_submissions):
-                    logging.info(
-                        f'    Observing submission "{submission.title}"')
-                    submission.comment_sort = 'new'
-                    try:
-                        submission.comments.replace_more(limit=depth_comments)
-                    except:
-                        logging.debug(f'  Cannot get more comments '
-                                      f'on submission {submission.id}')
-                    for comment in submission.comments.list():
-                        if comment.id not in cache:
-                            cache.append(comment.id)
-                            for keyword in keywords:
-                                keyword = keyword.lower()
-                                try:
-                                    if re.search(
-                                            f'(^{keyword} )|( {keyword} )',
-                                            comment.body):
-                                        logging.info(
-                                            f'      found {keyword} in '
-                                            f'comment {comment.id}')
-                                        occurences[keyword] += 1
-                                except:
-                                    logging.debug(
-                                        f'      Comment {comment.id} in '
-                                        f'submission {submission.id} '
-                                        f'doesn\'t have a body')
-                                    break
-                        else:
-                            logging.debug(
-                                f'      Skipping comment {comment.id} '
-                                f'(exists in cache)')
-            config['occurences'] = occurences
-            config['cache'] = cache
-            config['iterations'] = iterations
-            with open('config.json', 'w') as file:
-                json.dump(config, file, indent=2)
-            time.sleep(interval)
+            try:
+                iterations += 1
+                logging.info(f'Observer iteration {iterations}')
+                for subreddit in subreddits:
+                    logging.info(f'  Observing subreddit {subreddit}')
+                    for submission in reddit.subreddit(subreddit).new(
+                            limit=depth_submissions):
+                        logging.info(
+                            f'    Observing submission "{submission.title}"')
+                        submission.comment_sort = 'new'
+                        try:
+                            submission.comments.replace_more(limit=depth_comments)
+                        except:
+                            logging.debug(f'  Cannot get more comments '
+                                          f'on submission {submission.id}')
+                        for comment in submission.comments.list():
+                            if comment.id not in cache:
+                                cache.append(comment.id)
+                                for keyword in keywords:
+                                    keyword = keyword.lower()
+                                    try:
+                                        if re.search(
+                                                f'(^{keyword} )|( {keyword} )',
+                                                comment.body):
+                                            logging.info(
+                                                f'      found {keyword} in '
+                                                f'comment {comment.id}')
+                                            occurences[keyword] += 1
+                                    except:
+                                        logging.debug(
+                                            f'      Comment {comment.id} in '
+                                            f'submission {submission.id} '
+                                            f'doesn\'t have a body')
+                                        break
+                            else:
+                                logging.debug(
+                                    f'      Skipping comment {comment.id} '
+                                    f'(exists in cache)')
+                config['occurences'] = occurences
+                config['cache'] = cache
+                config['iterations'] = iterations
+                with open('config.json', 'w') as file:
+                    json.dump(config, file, indent=2)
+                time.sleep(interval)
+            except PrawcoreException:
+                logging.info(f'PRAW exception... Waiting 60 seconds')
+                time.sleep(60)
     except KeyboardInterrupt:
         config['occurences'] = occurences
         config['cache'] = cache
