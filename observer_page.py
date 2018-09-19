1e50938aac4a68da9dab5f3bbb87a816fb083763
Knut Zuidema <knut.zuidema@gmail.com>
2018-09-19 02:28:55 +0200

    added database access to flask app

--- observer_page.py
+++ observer_page.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import json
+import sqlite3
 
 app = Flask(__name__)
 
@@ -8,7 +9,17 @@ app = Flask(__name__)
 def occurrences():
     with open('config.json') as file:
         config = json.load(file)
-    data = config['occurences']
+    keywords = config['parameters']['keywords']
+    connection = sqlite3.connect('keywords.db')
+    cursor = connection.cursor()
+    data = dict()
+    for keyword in keywords:
+        try:
+            cursor.execute(f'select * from keywords where keyword=?',
+                           (keyword.lower(),))
+            data[keyword] = len(cursor.fetchall())
+        except sqlite3.OperationalError:
+            continue
     return render_template('index.html', data=data)
 
 
