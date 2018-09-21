bb40195676583e95669914a551f0927d3ee7a039
Knut Zuidema <knut.zuidema@gmail.com>
2018-09-22 00:28:21 +0200

    use input-group for search

--- static/js/search.js
+++ static/js/search.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $("#search").on("keyup", function () {
+    $("#searchbox").on("keyup", function () {
         const value = $(this).val().toLowerCase();
         $("#champion_table tbody tr").filter(function () {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
