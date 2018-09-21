266ab0ff4837cfb5a2a8858a538da43c5609fde7
Knut Zuidema <knut.zuidema@gmail.com>
2018-09-22 00:17:26 +0200

    implement search function

---
+++ static/js/search.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#search").on("keyup", function () {
+        const value = $(this).val().toLowerCase();
+        $("#champion_table tbody tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
