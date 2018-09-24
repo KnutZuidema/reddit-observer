b6ae3bb573fc6ac559349d42ee1b712aa9a6fedf
Knut Zuidema <knut.zuidema@gmail.com>
2018-09-25 01:27:46 +0200

    extract sql queries into functions and use max timestamp instead of current time on computer because of inconsistencies with the reddit api

--- observer_page.py
+++ observer_page.py
@@ -1,6 +1,5 @@
 import json
 import sqlite3
-import time
 from collections import defaultdict
 
 from flask import Flask, render_template
@@ -8,6 +7,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 DAY = 86400
+
+
+def utc_timestamp(cursor: sqlite3.Cursor):
+    cursor.execute('select max(timestamp) from keywords')
+    return cursor.fetchone()[0]
+
+
+def count_between(cursor: sqlite3.Cursor, keyword, lower: int = 0, upper: int = None):
+    upper = upper or utc_timestamp(cursor)
+    cursor.execute(f'select count(*) from keywords where '
+                   f'(keyword=?  collate nocase) '
+                   f'and timestamp between {lower} and {upper}',
+                   (keyword,))
+    return cursor.fetchone()[0]
 
 
 @app.route('/')
@@ -18,27 +31,18 @@ def occurrences():
     connection = sqlite3.connect('keywords.db')
     cursor = connection.cursor()
     data = defaultdict(dict)
-    now = time.time()
+    now = utc_timestamp(cursor)
     yesterday = now - DAY
     yesteryesterday = yesterday - DAY
     send = tuple()
     for keyword in keywords.keys():
         try:
-            cursor.execute('select count(*) from keywords '
-                           'where keyword=? collate nocase',
-                           (keyword,))
-            data[keyword]['all'] = cursor.fetchone()[0]
-            cursor.execute(f'select count(*) from keywords where '
-                           f'(keyword=?  collate nocase) '
-                           f'and timestamp between {yesterday} and {now}',
-                           (keyword,))
-            data[keyword]['day'] = cursor.fetchone()[0]
-            cursor.execute(f'select count(*) from keywords '
-                           f'where (keyword=?  collate nocase) '
-                           f'and timestamp between {yesteryesterday} '
-                           f'and {yesterday}',
-                           (keyword,))
-            change = data[keyword]['day'] - cursor.fetchone()[0]
+            data[keyword]['all'] = count_between(cursor, keyword)
+            data[keyword]['day'] = count_between(cursor, keyword,
+                                                 yesterday, now)
+            change = data[keyword]['day'] - count_between(cursor, keyword,
+                                                          yesteryesterday,
+                                                          yesterday)
             data[keyword]['change'] = change
             send = sorted(data.items(), key=lambda item: item[1]['day'],
                           reverse=True)
@@ -57,15 +61,11 @@ def occurrences():
 def keywords(keyword: str):
     connection = sqlite3.connect('keywords.db')
     cursor = connection.cursor()
-    upper = time.time()
+    upper = utc_timestamp(cursor)
     lower = upper - DAY
     data = list()
     for _ in range(7):
-        cursor.execute('select count(*) from keywords where '
-                       f'(keyword=? collate nocase) and '
-                       f'timestamp between {lower} and {upper}',
-                       (keyword,))
-        data = [cursor.fetchone()[0]] + data
+        data = [count_between(cursor, keyword, lower, upper)] + data
         upper = lower - 1
         lower -= DAY
     connection.close()
