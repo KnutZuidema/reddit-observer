6082f807a245f12d83d88bd451ca222d8c202b92
Knut Zuidema <knut.zuidema@gmail.com>
2018-09-28 02:13:36 +0200

    replace pure sqlite with sqlalchemy

--- observer_page.py
+++ observer_page.py
@@ -1,27 +1,14 @@
 import json
-import sqlite3
 from collections import defaultdict
 
+import sqlalchemy.exc
 from flask import Flask, render_template
+
+from database import get_session, Keyword, get_max, count, count_between
 
 app = Flask(__name__)
 
 DAY = 86400
-
-
-def max_timestamp(cursor: sqlite3.Cursor):
-    cursor.execute('select max(timestamp) from keywords')
-    return cursor.fetchone()[0]
-
-
-def count_between(cursor: sqlite3.Cursor, keyword, lower: int = 0,
-                  upper: int = None):
-    upper = upper or max_timestamp(cursor)
-    cursor.execute(f'select count(*) from keywords where '
-                   f'(keyword=?  collate nocase) '
-                   f'and timestamp between {lower} and {upper}',
-                   (keyword,))
-    return cursor.fetchone()[0]
 
 
 @app.route('/')
@@ -29,47 +16,46 @@ def occurrences():
     with open('config.json') as file:
         config = json.load(file)
     keywords = config['parameters']['keywords']
-    connection = sqlite3.connect('keywords.db')
-    cursor = connection.cursor()
+    session = get_session(config['database'])
     data = defaultdict(dict)
-    now = max_timestamp(cursor)
+    now = get_max(session, Keyword.timestamp)
     yesterday = now - DAY
     yesteryesterday = yesterday - DAY
     send = tuple()
     for keyword in keywords.keys():
         try:
-            data[keyword]['all'] = count_between(cursor, keyword)
-            data[keyword]['day'] = count_between(cursor, keyword,
-                                                 yesterday, now)
-            change = data[keyword]['day'] - count_between(cursor, keyword,
+            data[keyword]['all'] = count(session, Keyword.keyword, keyword)
+            data[keyword]['day'] = count_between(session, Keyword.keyword,
+                                                 yesterday, now, keyword)
+            change = data[keyword]['day'] - count_between(session,
+                                                          Keyword.keyword,
                                                           yesteryesterday,
-                                                          yesterday)
+                                                          yesterday, keyword)
             data[keyword]['change'] = change
             send = sorted(data.items(), key=lambda item: item[1]['day'],
                           reverse=True)
-        except sqlite3.OperationalError:
+        except sqlalchemy.exc.OperationalError:
             continue
-    connection.close()
     total = {
-        'all': sum(keyword['all'] for keyword in data.values()),
-        'day': sum(keyword['day'] for keyword in data.values()),
-        'change': sum(keyword['change'] for keyword in data.values())
+        'all': count(session, Keyword),
+        'day': count_between(session, Keyword, yesterday, now),
+        'change': count_between(session, Keyword, yesteryesterday, yesterday)
     }
     return render_template('index.html', data=send, total=total)
 
 
 @app.route('/<keyword>')
 def keywords(keyword: str):
-    connection = sqlite3.connect('keywords.db')
-    cursor = connection.cursor()
-    upper = max_timestamp(cursor)
+    with open('config.json') as file:
+        config = json.load(file)
+    session = get_session(config['database'])
+    upper = get_max(session, Keyword.timestamp)
     lower = upper - DAY
     data = list()
     for _ in range(7):
-        data = [count_between(cursor, keyword, lower, upper)] + data
+        data = [count_between(session, Keyword, lower, upper)] + data
         upper = lower - 1
         lower -= DAY
-    connection.close()
     return render_template('keyword.html', keyword=keyword, data=data)
 
 
