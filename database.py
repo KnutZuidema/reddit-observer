f1d4574cb0d50d99d5fa48fa39124a2a70750a10
Knut Zuidema <knut.zuidema@gmail.com>
2018-10-04 02:28:13 +0200

    implement method to query mentions per submission

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
-from typing import Union, Any, Type, Dict
+from collections import defaultdict
+from typing import Union, Any, Type, Dict, List, Tuple
 
 from sqlalchemy import engine_from_config, Column, Integer, String, func
 from sqlalchemy.engine.base import Engine
@@ -41,6 +42,16 @@ class SQLSession:
         query = self.session.query(column)
         query = query.filter(column.collate('nocase') == comparison)
         return query.filter(Mention.timestamp.between(lower, upper)).count()
+
+    def get_submissions(self, keyword: str) -> List[Tuple[str, str, int]]:
+        query = self.session.query(Mention.permalink)
+        query = query.filter(Mention.keyword.collate('nocase') == keyword)
+        submissions = defaultdict(int)
+        for permalink in query:
+            link = permalink.rsplit('/', maxsplit=2)[0]
+            title = permalink.split('/')[4].replace('_', ' ')
+            submissions[(link, title)] += 1
+        return [(link, title, mentions) for (link, title), mentions in submissions.items()]
 
 
 Base = declarative_base()
